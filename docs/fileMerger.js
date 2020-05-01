acbf31b9b285603acf167197327f278eae009e24
cJayesh <chavanjayesh63@gmail.com>
2020-05-02 00:37:27 +0530

    Add drag and drop

--- docs/fileMerger.js
+++ docs/fileMerger.js
@@ -1,23 +1,38 @@
-const allowedExtensions = ['txt', 'sql'];
-
 function run() {
-    document.getElementById('inputFile').addEventListener('change', function(){
-        document.getElementById('copied').classList.add('hide');
-        const comment = '-'.repeat(10);
-        const output = document.getElementById('outputFile');
-        const fileNames = document.getElementById('fileNames');
-        output.value = '';
-        fileNames.textContent = '';
+    document.getElementById('inputFile').addEventListener('change', handleFile);
+    document.getElementById('outputTextArea').addEventListener('drop', handleDroppedFile);
+}
 
-        let filePromises = Array.from(this.files).map(file => readFile(file));
-        Promise.all(filePromises)
-            .then(responses => {
-                responses.forEach(response => {
-                    output.value += comment.concat(response.name, comment, '\n');
-                    output.value += response.text.concat('\n\n\n');
-                    fileNames.textContent += response.name.concat('\n');
-                });
-            });
+function handleDroppedFile(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    const files = event.dataTransfer.files;
+    processFiles(files);
+}
+
+function handleFile() {
+    processFiles(this.files);
+}
+
+function processFiles(files) {
+    const uploadedFiles = files;
+    document.getElementById('copied').classList.add('hide');
+    const comment = '-'.repeat(10);
+    const output = document.getElementById('outputTextArea');
+    const fileNames = document.getElementById('fileNames');
+    output.value = '';
+    fileNames.textContent = '';
+
+    let filePromises = Array.from(uploadedFiles).map(file => readFile(file));
+    Promise.all(filePromises)
+    .then(responses => {
+        responses.forEach(response => {
+            if (response.name.split('.').pop() === "sql") {
+                output.value += comment.concat(response.name, comment, '\n');
+                output.value += response.text.concat('\nGO\n\n\n');
+                fileNames.textContent += response.name.concat('\n');
+            }
+        });
     });
 }
 
@@ -33,7 +48,7 @@ function readFile(file) {
 }
 
 function copyToClipboard() {
-    const selection = document.getElementById('outputFile');
+    const selection = document.getElementById('outputTextArea');
     selection.select();
     document.execCommand('copy');
     document.getElementById('copied').classList.remove('hide');
@@ -41,7 +56,7 @@ function copyToClipboard() {
 
 function reset() {
     document.getElementById('inputFile').value = '';
-    document.getElementById('outputFile').value = '';
+    document.getElementById('outputTextArea').value = '';
     document.getElementById('fileNames').textContent = '';
     document.getElementById('copied').classList.add('hide');
 }
